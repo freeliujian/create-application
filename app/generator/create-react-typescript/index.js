55220671f8bc89fa0568a0496e5bf13363c97062
littleforest <670177821@qq.com>
2022-08-09 09:18:34 +0800

    新增react官方脚手架ts模板

---
+++ app/generator/create-react-typescript/index.js
@@ -0,0 +1,72 @@
+const Generator = require('yeoman-generator')
+const BasicGenerator = require('../../BasicGenerator')
+const {getNameByRepository} = require('../../utils')
+const {existsSync,readFileSync} =require('fs')
+
+module.exports = class Generator extends BasicGenerator {
+  prompting() {
+    return this.prompt([
+      {
+        name:'repository',
+        message:'请输入你的git仓库地址(eg:xxxx)'
+      },
+      {
+        type:'input',
+        name:'envPort',
+        default:'8000',
+        message:'请输入的你端口号:（默认:8000)'
+      },
+    ])
+      .then(props => {
+        let gitModules = []
+       
+        const {name,moduleName,packageName} = getNameByRepository(props.repository)
+        try {
+          const gitModulesContent = readFileSync(this.destinationPath('.gitmodules'), {
+            encoding: 'utf-8',
+          });
+          
+          gitModules =
+            typeof gitModulesContent === 'string' &&
+            gitModulesContent
+              .split('\n')
+              .filter((i) => i.startsWith('[') && i.endsWith(']'))
+              .map((i) => i.split('/')[1].replace('"]', ''));
+              
+        } catch (e) {
+         
+          throw new Error('解析.gitmodules失败!');
+        }
+        if (
+          existsSync(this.destinationPath(this.perfix, moduleName)) ||
+          gitModules.indexOf(moduleName) > -1
+        ) {
+          throw new Error('该模块已存在');
+        }
+        
+        this.props = {...props,name,moduleName,packageName}
+      })
+  }
+
+  default() {
+    console.log('clone submodule...');
+    const path =
+      this.perfix.length > 0
+        ? `${this.perfix}/${this.props.moduleName}`
+        : this.props.moduleName;
+   
+    this.spawnCommandSync('git', ['clone', this.props.repository, path]);
+  }
+
+  writing() {
+    const {features} = this.props;
+    const isGulp = (file) => {
+      if(!features.includes('gulp')){
+        return file != 'gulpfile.js'
+      }else{
+        return true
+      }
+    }
+    this.writeFiles({context:this.props, filterFiles:isGulp})
+  }
+}
